39b765ba9ca38dfd70e4760acadbd5db9bfefc7a
regan preston <regan.preston@f8th.ai>
2022-03-02 20:19:50 -0700

    completed insert

--- lab6/src/main/java/ca/sait/lab6/dataaccess/UserDB.java
+++ lab6/src/main/java/ca/sait/lab6/dataaccess/UserDB.java
@@ -84,4 +84,34 @@ public class UserDB {
         return user;
     }
 
+    public boolean insert(User user) throws Exception{
+
+            ConnectionPool cp = ConnectionPool.getInstance();
+            Connection con = cp.getConnection();
+            PreparedStatement ps = null;
+
+            String sql = "INSERT INTO `userdb`.`user` (`email`, `first_name`, `last_name`, `password`, `role`) VALUES (?, ?, ?, ?, ?)";
+
+            boolean inserted;
+
+            try{
+                ps = con.prepareStatement(sql);
+                ps.setString(1, user.getEmail());
+                ps.setString(2, user.getFirstName());
+                ps.setString(3, user.getLastName());
+                ps.setString(4, user.getPassword());
+                ps.setInt(5, user.getRole().getId());
+                inserted = ps.executeUpdate() != 0;
+            }finally{
+                DBUtil.closePreparedStatement(ps);
+                cp.freeConnection(con);
+            }
+
+            return inserted;
+        }
+
+        public void update() throws Exception{
+
+        }
+
 }
